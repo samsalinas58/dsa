b1d570ac7ffca648b2f8e5f2baa318b36e3d6efb
Samuel Salinas <samuelsalinas574@gmail.com>
2025-05-26 14:52:38 -0500

    basic queue implemented yippie

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -1,18 +1,33 @@
 package Arrays;
 
 import Arrays.Stack;
+import Arrays.Queue;
 
 class Main {
     public static void main(String[] args) {
-        Stack s = new Stack(1);
-        s.print();
-        s.push(2).push(3).push(4);
-        s.print();
+     //   Stack s = new Stack(1);
+     //   s.print();
+     //   s.push(2).push(3).push(4);
+     //   s.print();
 
-        System.out.println(s.top());
-        s.pop().pop().pop().pop();
-        s.print();
+     //   System.out.println(s.top());
+     //   s.pop().pop().pop().pop();
+     //   s.print();
 
-        s.pop(); // EmptyStackException!
+     //   s.pop(); // EmptyStackException!
+
+        Queue q = new Queue(1);
+        q.print();
+
+        q.push(2).push(3).push(4);
+        q.pop();
+        q.push(1);
+        q.push(5);
+        q.pop();
+        q.push(6);
+        q.print();
+        q.pop().pop().pop().pop().pop();
+        q.print();
+
     }
 }
--- src/Arrays/Queue.java
+++ src/Arrays/Queue.java
@@ -1,5 +1,81 @@
 package Arrays;
 
 class Queue {
+    int[] arr = new int[1];
+    int front = -1;
+    int back = -1;
+
+    Queue() {
+        arr = new int[1];
+    }
+
+    Queue(int value) {
+        front = 0;
+        back = 0;
+        arr[0] = value;
+    }
+
+    public int size() {
+        if (front == -1) return 0;
+        if (back < front) return (arr.length - front) + (back + 1);
+        return (back - front) + 1;
+    }
+
+    public int front() { return arr[front]; }
+
+    public Queue push(int value) {
+        if (front == -1) {
+            arr[0] = value;
+            front = 0;
+            back = 0;
+            return this;
+        }
+        back += 1;
+        if (back == arr.length) back = 0;
+        if (back == front) {
+            int capacity = arr.length * 2;
+            int[] newArr = new int[capacity];
+            for (int i = front, ct = 0; ct < arr.length; ct++, i++) {
+                if (i == arr.length) i = 0;
+                newArr[ct] = arr[i];
+            }
+            back = arr.length;
+            front = 0;
+            arr = newArr;
+            arr[back] = value;
+            return this;
+        }
+        arr[back] = value;
+        return this;
+    }
+
+    public Queue pop() {
+        if (front == -1) return this;
+        if (front == back) {
+            front = -1;
+            back = -1;
+            return this;
+        }
+        front += 1;
+        if (front == arr.length) front = 0;
+        return this;
+    }
+
+    public void print() {
+        int size = this.size();
+        if (this.size() == 0) {
+            System.out.println("[]");
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = front, ct = 0; ct < size; i++, ct++) {
+            if (i == arr.length) i = 0;
+            sb.append(arr[i]).append(", ");
+        }
+        sb.setLength(sb.length() - 2);
+        sb.append("]");
+        System.out.println(sb);
+    }
 
 }
