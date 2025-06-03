6fa257bdc7657beb4768c38aaf4799c6788a0383
Samuel Salinas <samuelsalinas574@gmail.com>
2025-06-03 13:44:17 -0500

    AVLTree insertion and deletion test cases

--- test/binarytree/Test.java
+++ test/binarytree/Test.java
@@ -24,11 +24,23 @@ import main.binarytree.*;
 
 class AVLTreeTest {
 
+    // For the insertion tests, the constructor directly calls insert() when constructing the tree
+    // using an array as the parameter.
     @Test
     void testAVLTreeEmptyInsertion() {
         int[] expected = {1};
         AVLTree a = new AVLTree();
         a.insert(1);
+
+        assert(a.equals(expected));
+    }
+
+    @Test
+    void testAVLTreeInsertion_valueAlreadyExists() {
+        int[] tree = {100,95,105,96,104};
+        int[] expected = {100,95,96,105,104};
+        AVLTree a = new AVLTree(tree);
+        for (int i: tree) a.insert(i);
 
         assert(a.equals(expected));
     }
@@ -104,6 +116,145 @@ class AVLTreeTest {
         int[] tree = {55,50,60,62,61};
         int[] expected = {55,50,61,60,62};
         AVLTree a = new AVLTree(tree);
+
+        assert(a.equals(expected));
+    }
+
+    @Test
+    void testAVLTreeDeletion_emptyTree() {
+        int[] expected = {};
+        AVLTree a = new AVLTree();
+
+        a.delete(100);
+        assert(a.equals(expected));
+    }
+
+    @Test
+    void testAVLTree_valueDoesntExist() {
+        int[] tree = {100,95,105,94,106};
+        int[] expected = {100,95,94,105,106};
+        AVLTree a = new AVLTree(tree);
+        a.delete(97);
+        a.delete(98);
+
+        assert(a.equals(expected));
+    }
+
+    @Test
+    void testAVLTreeDeletion_rootWIthNoSubTree() {
+        int[] expected = {};
+        AVLTree a = new AVLTree(100);
+        a.delete(100);
+
+        assert(a.equals(expected));
+    }
+
+    @Test
+    void testAVLTreeDeletion_rootWithChildNoRotation() {
+        int[] tree = {100,95};
+        int[] expected = {95};
+        AVLTree a = new AVLTree(tree);
+        a.delete(100);
+
+        assert(a.equals(expected));
+    }
+
+    @Test
+    void testAVLTreeDeletion_leafNoRotation() {
+        int[] tree = {100,95,105,90,97};
+        int[] expected = {100,95,90,105};
+        AVLTree a = new AVLTree(tree);
+        a.delete(97);
+
+        assert(a.equals(expected));
+    }
+
+    @Test
+    void testAVLTreeDeletion_rootSubtreeNoRotation() {
+        int[] tree = {100,95,105,90,97};
+        int[] expected = {97,95,90,105};
+        AVLTree a = new AVLTree(tree);
+        a.delete(100);
+
+        assert(a.equals(expected));
+    }
+
+    @Test
+    void testAVLTreeDeletion_leftRotationAtRoot() {
+        int[] tree = {100, 95, 105, 108};
+        int[] expected = {105,100,108};
+        AVLTree a = new AVLTree(tree);
+        a.delete(95);
+
+        assert(a.equals(expected));
+    }
+
+    @Test
+    void testAVLTreeDeletion_rightRotationAtRoot() {
+        int[] tree = {100,95,105,90};
+        int[] expected = {95,90,100};
+        AVLTree a = new AVLTree(tree);
+        a.delete(105);
+
+        assert(a.equals(expected));
+    }
+
+    @Test
+    void testAVLTreeDeletion_rightLeftRotationAtRoot() {
+        int[] tree = {100,99,105,103};
+        int[] expected = {103,100,105};
+        AVLTree a = new AVLTree(tree);
+        a.delete(99);
+
+        assert(a.equals(expected));
+    }
+
+    @Test
+    void testAVLTreeDeletion_leftRightRotationAtRoot() {
+        int[] tree = {100,105,97,98};
+        int[] expected = {98,97,100};
+        AVLTree a = new AVLTree(tree);
+        a.delete(105);
+
+        assert(a.equals(expected));
+    }
+
+    @Test
+    void testAVLTreeDeletion_subtreeLeftRotation() {
+        int[] tree = {100,105,95,90,101,110,111};
+        int[] expected = {101,95,90,110,105,111};
+        AVLTree a = new AVLTree(tree);
+        a.delete(100);
+
+        assert(a.equals(expected));
+    }
+
+    @Test
+    void testAVLTreeDeletion_subtreeRightRotation() {
+        int[] tree = {100,105,95,90,104,110,103};
+        int[] expected = {100,95,90,104,103,105};
+        AVLTree a = new AVLTree(tree);
+        a.delete(110);
+
+        assert(a.equals(expected));
+    }
+
+    @Test
+    void testAVLTreeDeletion_subtreeLeftRightRotation() {
+        int[] tree = {100,105,95,90,103,110,104};
+        int[] expected = {100,95,90,104,103,105};
+        AVLTree a = new AVLTree(tree);
+        a.delete(110);
+
+        assert(a.equals(expected));
+    }
+
+    @Test
+    void testAVLTreeDeletion_subTreeRightLeftRotation() {
+        int[] tree = {100,90,110,80,101,120,115};
+        int[] expected = {101,90,80,115,110,120};
+        AVLTree a = new AVLTree(tree);
+        a.delete(100);
 
         assert(a.equals(expected));
     }
